434acfacf42e9ca6d09b87da7c18f7378835edfc
Francois Boschet <fboschet@insa-rennes.fr>
2015-04-23 12:20:08 +0200

    New feature!
    Connexion automatique a un pc dispo :smile_cat:

--- fast.py
+++ fast.py
@@ -12,12 +12,10 @@ def fill (n) :
         i += 1
     return res
 
-
-os.system("""cat host.educ.insa | cut -d " " -f 1 > host.list""")
-
 hosts =  open("depart.list", 'r').read().split("\n")[:-1]
 
 up = 0
+uplist = []
 down = 0
 
 #hosts = ["cavalier.educ.insa", "10.133.26.251"]
@@ -31,9 +29,32 @@ for hostname in hosts :
     if response == 0:
         up += 1
         print "\t|\033[92m", hostname, fill(tab-len(hostname)), "\033[0m|is up  |"
+        uplist += [hostname]
     else:
         down += 1
         print "\t|\033[91m", hostname, fill(tab-len(hostname)), "\033[0m|is down|"
     print "\t+------------------------------+-------+"
 
 print "up ->", up, "down ->", down
+
+b = ""
+while b != "n" and b != "y" and b != "a":
+    b = raw_input("Do you want to connect to a pc?[y/n/a] ")
+
+if b == "y":
+    usr = raw_input("username? ")
+    os.system("ssh "+usr+"@"+uplist[0])
+
+elif b == "a":
+    pc = ""
+    ok = False
+    while not ok :
+        pc= raw_input("PC name? ")
+        if uplist.count(pc+".educ.insa") != 1 :
+            print "Unavailable"
+        else :
+            ok = True
+    usr = raw_input("username? ")
+    os.system("ssh "+usr+"@"+pc+".educ.insa")
+
+print "bye"
